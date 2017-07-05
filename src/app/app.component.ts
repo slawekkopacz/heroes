7e4133333fe95fea574d6407d7d02b9f0a3a6bf8
slawekkopacz <s.kopacz@gmail.com>
2017-07-05 17:49:51 +0200

    two-way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  heroes: Hero[] = [
+    {
+      id: 0,
+      name: 'Fearless',
+    },
+    {
+      id: 1,
+      name: 'Greatest',
+    },
+  ];
+  hero: Hero = this.heroes[0];
 }
