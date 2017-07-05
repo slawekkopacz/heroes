bd47ae68e6f0ba41d41f7ae45f4308f146d00ec9
slawekkopacz <s.kopacz@gmail.com>
2017-07-05 18:28:33 +0200

    selecting from list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ export class Hero {
   name: string;
 }
 
+const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+  ];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,15 +25,10 @@ export class Hero {
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  heroes: Hero[] = [
-    {
-      id: 0,
-      name: 'Fearless',
-    },
-    {
-      id: 1,
-      name: 'Greatest',
-    },
-  ];
-  hero: Hero = this.heroes[0];
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
